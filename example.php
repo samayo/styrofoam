5196c529e06c87f345dfe2e3dcdebc779f382b5b
simon-eQ <Eritrea@users.noreply.github.com>
2013-10-04 17:59:48 +0200

    Update example.php

--- example.php
+++ example.php
@@ -1,50 +1,42 @@
 <?php 
 
-/*
- * this demonstrates how to do a simple and basic CRUD with NoodlePiece
- *
- */
+
+  // this demonstrates how to do a simple and basic CRUD with NoodlePiece
+
+
  
  
   $db = new PDO('mysql:host=localhost; dbname=eritrean_lib', 'root', '');  // this is just an example
 	
-	
-	
   require_once 'NoodlePiece.php';
 	
- 	$NoodlePiece = new NoodlePiece($db);
- 	
- 	
- 	
+	$NoodlePiece = new NoodlePiece($db);
+	
 	
 	/* how to do a SELECT statement */
 	
 	$select = $NoodlePiece->doLazy('SELECT * FROM questions')->where('id = ?', [1]);
 	
-	
-	
 	/* how to do a INSERT statement */
-
-	 $insert = $NoodlePiece->doLazy('INSERT INTO questions (name, job)') ->values('(?,?)', ['simon', 'developer'], true);
-	 
-	 
-	 
+	
+	$insert = $NoodlePiece->doLazy('INSERT INTO questions (name, job)') ->values('(?,?)', ['simon', 'developer'], true);
+	
+	
 	/* how to do a UPDATE statement */
-
- $update = $NoodlePiece->doLazy('UPDATE questions')->set('choice_1 = ? WHERE id = ?', ['orange', 1]);
- 
- 
- 
+	
+	$update = $NoodlePiece->doLazy('UPDATE questions')->set('choice_1 = ? WHERE id = ?', ['orange', 1]);
+	
+	
 	/* how to do a DELETE statement */
-
+	
 	$delete = $NoodlePiece->doLazy('DELETE FROM questions')->where('id = ?', [6]);
 	
-
-						  				
+	
+				  				
 	
 	
-	 
-
 	
- 
+	
+	
+	
 	
