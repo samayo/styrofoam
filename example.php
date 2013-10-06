5c3ef555b308c2a1db3246596b1b23c601b8bf18
simon-eQ <Eritrea@users.noreply.github.com>
2013-10-07 00:33:37 +0200

    Update example.php

--- example.php
+++ example.php
@@ -20,22 +20,22 @@
 	/*
 	 * How to do a SELECT statement
 	 */
-	$select = $NoodlePiece->doLazy('SELECT * FROM questions')->where('id = ?', [1]);
+	$select = $NoodlePiece->doLazy('SELECT * FROM users')->where('name = ?', ['jimmy']);
 	
 	/*
-	 * How to do a INSERT statement
+	 * How to do a INSERT statement | 3rd parameter = 'true' returnes lastInsertId()
 	 */
-	$insert = $NoodlePiece->doLazy('INSERT INTO questions (name, job)') ->values('(?,?)', ['simon', 'developer'], true);
+	$insert = $NoodlePiece->doLazy('INSERT INTO employees (name, job)') ->values('(?,?)', ['simon', 'developer'], true);
 	
 	/*
 	 * How to do a UPDATE statement
 	 */
-	$update = $NoodlePiece->doLazy('UPDATE questions')->set('choice_1 = ? WHERE id = ?', ['orange', 1]);
+	$update = $NoodlePiece->doLazy('UPDATE car_type')->set('jaguar = ? WHERE id = ?', ['ferrari', 1]);
 	
 	/*
 	 * How to do a DELETE statement
 	 */
-	$delete = $NoodlePiece->doLazy('DELETE FROM questions')->where('id = ?', [6]);
+	$delete = $NoodlePiece->doLazy('DELETE FROM vegetables')->where('id = ?', [16]);
 	
 	
 				  				
