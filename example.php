13257ea0d745945bc8cb358699780ef0c4069f0b
Simon <Eritrea@users.noreply.github.com>
2013-11-11 21:16:39 +0200

    Update example.php

--- example.php
+++ example.php
@@ -1,28 +1,34 @@
 <?php
 
 
-require_once 'PdoWrapper.php';
+	require_once 'PdoWrapper.php';
 
 
 	// Create a new PdoWrapper instance and do parameterized queries
-	$db = new PdoWrapper('mysql:host=localhost; dbname=habeshaCity', 'root', '',
-	array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+	$db = new PdoWrapper('mysql:host=localhost; dbname=mydb', 'root', '');
 
 
 
 	
-	// How to perform simple CRUD statements. 
-	$select = $db->simple("SELECT * FROM girls WHERE name = ?", array('Megan Fox'));
+	//How to perform simple CRUD statements. 
+	$select = $db->doSimple("SELECT * FROM girls WHERE name = ?", array('Megan Fox'), $error);
 	
-	$insert = $db->simple("INSERT INTO employees (name, job) VALUES (?,?)", array('simon', 'sarcasm'));
+	$insert = $db->doSimple("INSERT INTO employees (name, job) VALUES (?,?)", array('simon', 'sarcasm'), $error);
 	
-	$update = $db->simple("UPDATE season SET winter = ? WHERE id = ?", array('summer', 1));
+	$update = $db->doSimple("UPDATE relationship SET single = ? WHERE money = ?", array('single', 'flowing'), $error);
 	
-	$delete = $db->simple("DELETE FROM clients WHERE id = ?", array('666'));
+	$delete = $db->doSimple("DELETE FROM clients WHERE id = ?", array('666'), $error);
 	
 	//same goes for simple queries, without parameters 
-	$select = $db->simple("SELECT * FROM foo"); 
+	$select = $db->doSimple("SELECT * FROM emails", $error); 
+
 	
+	//later you will be able to check for errors on the fly, instead of using try/catch blocks
+	if(!$error){
+		// move to the next levl
+	}else{
+		echo 'ERROR FOUND: '.$error; 
+	}
 
 
 
