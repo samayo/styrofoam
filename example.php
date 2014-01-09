586d8b37f8f4f57b1f4058c03652802dbb95276d
oO <Eritrea@users.noreply.github.com>
2014-01-09 11:28:25 +0100

    Update example.php

--- example.php
+++ example.php
@@ -11,16 +11,16 @@
 
 
 	//How to perform simple CRUD statements.
-	$select = $db->doSimple("SELECT * FROM girls WHERE name = ?", array('Megan Fox'), $error);
+	$select = $db->wrap("SELECT * FROM girls WHERE name = ?", array('Megan Fox'), $error);
 
-	$insert = $db->doSimple("INSERT INTO employees (name, job) VALUES (?,?)", array('simon', 'sarcasm'), $error);
+	$insert = $db->wrap("INSERT INTO employees (name, job) VALUES (?,?)", array('simon', 'sarcasm'), $error);
 
-	$update = $db->doSimple("UPDATE relationship SET taken = ? WHERE money = ?", array('single', 'flowing'), $error);
+	$update = $db->wrap("UPDATE relationship SET taken = ? WHERE money = ?", array('single', 'flowing'), $error);
 
-	$delete = $db->doSimple("DELETE FROM clients WHERE id = ?", array('666'), $error);
+	$delete = $db->wrap("DELETE FROM clients WHERE id = ?", array('666'), $error);
 
 	//same goes for simple queries, without parameters
-	$select = $db->doSimple("SELECT * FROM emails", $error);
+	$select = $db->wrap("SELECT * FROM emails", $error);
 
 
 	//later you will be able to check for errors on the fly, instead of using try/catch blocks
