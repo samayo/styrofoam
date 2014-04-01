f30fc43c032fbe154675854b4e58fbaab7db7f42
simon|er <equbazghi@yahoo.com>
2014-04-02 00:11:32 +0200

    Placed the wrapper in a separate src folder

---
+++ src/PdoWrapper.php
@@ -0,0 +1,81 @@
+<?php
+/**
+ * PdoWrapper: A small class to wrap PDO's prepare/query methods
+ * @author     Simon _eQ <https://github.com/simon-eQ>
+ * @license    Public domain. Do anything you want with it.
+ *
+ */
+
+class PdoWrapper extends PDO
+{
+    public function __construct($dsn, $user, $pass, $options = null)
+    {
+        try{
+            parent::__construct($dsn, $user, $pass, $options = null);
+        }catch(PDOException $e){
+            die($e->getMessage());
+        }
+    }
+
+    /**
+     * Main query builder method. accepts the statement!
+     *
+     * @param $query  Build your statement query here
+     * @param null $value pass the values as array for the execute() method
+     * @param null $error assign error for later error checking.
+     * @return $this|array if query was a SELECT statement, then we'll get the $stmt object
+     */
+    public function wrap($query, $value = null, &$error = null)
+    {
+        /**
+         *  if $value is empty/null, then do a query() statement only
+         */
+        if(!$value)
+        {
+            if(parent::query($query) == false)
+            {
+                $error = 'Query failed. Use propery try/catch method to capture the error';
+            }
+           
+             /**
+              * if there aren't any errors, return the PDO statement itself.
+              */
+            return parent::query($query);
+        }
+
+
+        /**
+         *  if $value is not empty, then use prepare method, by passing the $value to be executed
+         */
+        $stmt = parent::prepare($query);
+        $stmt->execute($value);
+
+
+        /**
+         * 
+         * Checking for string 'SELECT' allows us to know what kind of query was passed
+         * therefore, if it was SELECT we can return the $stmt object
+         */
+        if(stripos('SELECT', $query) < 5)
+        {
+            if((int)$stmt->errorCode()){
+                $error = $stmt->errorInfo();
+            }
+            return $error ? $this : $stmt;
+        }
+        
+
+        /**
+         * remeber errorCode gives '0000' if there are no errors, so only
+         * casting it to int will give '0' or meaning there wasn't any error. 
+         * othewise $error will be initialized. 
+         */
+         
+           if((int)$stmt->errorCode()){
+                $error = $stmt->errorInfo();
+            }
+         return $this;
+
+    }
+
+}
